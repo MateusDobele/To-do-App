8deb38814c965c70dd77e7f171eef8ea4a7d3f02
mateus.dobele@hotmail.com <96268873+MateusDobele@users.noreply.github.com>
2025-02-16 19:48:21 +0100

    Implement the "Complete" & "Exit" button

--- GUI.py
+++ GUI.py
@@ -5,13 +5,17 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45,10])
-edit_button= sg.Button("Edit")
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
 # List of lists to construct the GUI window layout:
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+layout = [[label],
+          [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 
-# 'layout' command is used to connect the 'label', 'input_box', 'add_button', etc to the GUI window
+# 'layout' command is used to connect the 'label', 'input_box', etc. to the GUI window
 window = sg.Window('My To-Do App',
                    layout=layout,
                     font=('Helvetica', 20))
@@ -37,6 +41,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
